48dce47c8a6340bfc4cabba6e706ecccb3e81da5
Deepak Bhamla <dbhamla7@gmail.com>
2021-06-22 20:59:56 +0530

    another bug fix for deploy

--- project/settings.py
+++ project/settings.py
@@ -135,7 +135,7 @@ STATICFILES_DIRS = (
     os.path.join(BASE_DIR, "static"),
 )
 
-STATIC_ROOT = os.path.join(BASE_DIR, "your_static_folder")
+STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
 
 #media files
 MEDIA_ROOT=os.path.join(BASE_DIR,'media')
