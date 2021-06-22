b111a4d2ece516f32a7fdd035d37fd3593e7fe0c
Deepak Bhamla <dbhamla7@gmail.com>
2021-06-22 21:02:35 +0530

    heroku css missing

--- project/settings.py
+++ project/settings.py
@@ -135,7 +135,7 @@ STATICFILES_DIRS = (
     os.path.join(BASE_DIR, "static"),
 )
 
-STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
+# STATIC_ROOT = os.path.join(BASE_DIR, "static")
 
 #media files
 MEDIA_ROOT=os.path.join(BASE_DIR,'media')
